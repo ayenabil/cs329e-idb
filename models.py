a68471d224c8c817ee23cf98bf4805489b36d6aa
ayenabil <ahmedalnabil@utexas.edu>
2017-10-17 02:35:31 -0500

    Final

--- models.py
+++ models.py
@@ -1,14 +1,34 @@
-from flask import flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test123@localhost/tutorialdb'
 db = SQLAlchemy(app)
 
-class Heroes(db):
+class Heroes(db.Model):
     __tablename__ = 'heroes'
-class Creators(db):
-    __tablename__ = 'creators'
-class Series(db):
-    __tablename__ = 'series'
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    details = db.Column(db.String(120), nullable=False)
+    wiki = db.Column(db.String(120), nullable=False)
+    #series = db.Column()
 
+class Creators(db.Model):
+    __tablename__ = 'creators'
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    details = db.Column(db.String(120), nullable=False)
+    comics = db.Column(db.String(80), nullable=False)
+    #series = db.Column()
+
+class Series(db.Model):
+    __tablename__ = 'series'
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    startyear = db.Column(db.Integer, primary_key=True)
+    endyear = db.Column(db.Integer, primary_key=True)
+    #creators = db.Column()
+    #characters = db.Column()
+
+db.drop_all()
+db.create_all()
